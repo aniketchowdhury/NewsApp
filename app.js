75780cbcfb487006151d10139008e9723aec077a
aniketchowdhury <chowdhury.aniket14@gmail.com>
2016-12-20 12:04:39 +0000

    server side

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
 var news = require('./routes/news');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var connectflash = require('connect-flash');
+var User = require('./models/usersmodel.js')
+
+
 //var app = express();
 
 var mongoose = require('mongoose');
@@ -18,6 +24,7 @@ db.on('error',console.error.bind(console,'connection error'));
 db.once('open',function(){
 console.log("connected to mongodb successfully");
 });
+
 
 var webpackDevMiddleware = require("webpack-dev-middleware");
 var webpack = require("webpack");
@@ -46,10 +53,14 @@ app.use(webpackHotMiddleware(compiler, {
 }))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -60,6 +71,27 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/login',login);
 app.use('/news',news);
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username , password: password }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+    //  if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
